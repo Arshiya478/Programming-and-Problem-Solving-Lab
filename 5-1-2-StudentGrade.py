b35565de8b3181243442c344a481094c147ba70a
Arshiya <arshiyakayarkar@gmail.com>
2026-05-22 21:26:40 +0530

    5.1.2. Student Grade Based on Aggregate

---
+++ 5-1-2-StudentGrade.py
@@ -0,0 +1,20 @@
+#Student Grade Based on Aggregate
+
+marks = list(map(int, input().split()))
+total = sum(marks)
+aggregate = (total / 400) * 100
+
+if aggregate >= 75:
+	grade = "Distinction"
+elif aggregate >= 60:
+	grade = "First Division"
+elif aggregate >= 50:
+	grade = "Second Division"
+elif aggregate >= 40:
+	grade = "Third Division"
+else:
+	grade = "Fail"
+
+print(total)
+print(f"{aggregate:.2f}")
+print(grade)
